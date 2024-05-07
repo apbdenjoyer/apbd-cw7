106565d486f79075679d5614d52ab0c660e2d79f
Franciszek Gozdecki <s27379@pjwstk.edu.pl>
2024-05-07 04:47:24 +0200

    cleared project

--- apbd-cw7/apbd-cw7/Models/DTOs/NewProductInWarehouse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace apbd_cw7.Models.DTOs;
-
-public class NewProductInWarehouse
-{
-    public int IdProduct { get; set; }
-    public int IdWarehouse { get; set; }
-    public int Amount { get; set; }
-    public DateTime CreatedAt { get; set; }
-}
